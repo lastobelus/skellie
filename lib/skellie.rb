69ca6ce5f5b63f89b07ed1409ac7d6e2933f3e3b
Michael Johnston <lastobelus@mac.com>
2020-02-25 13:24:35 -0800

    add Skellie#config and Skellie#configure

--- lib/skellie.rb
+++ lib/skellie.rb
@@ -1,6 +1,42 @@
 require "skellie/version"
+require "yaml"
 
 module Skellie
   class Error < StandardError; end
-  # Your code goes here...
+
+  @config = {
+    log_level: "verbose",
+    models: {
+      type_aliases: {
+        integer: %W[i int],
+        decimal: %w[dec],
+        float: %w[f],
+        boolean: %w[boo bool],
+        binary: [],
+        string: %w[s str],
+        text: %w[t tx txt],
+        date: %w[d],
+        time: [],
+        datetime: %w[dt],
+        references: %w[ref refs],
+      },
+    },
+  }
+
+  @valid_config_keys = @config.keys
+
+  # Configure through hash
+  def self.configure(opts = {})
+    opts.each { |k, v| @config[k.to_sym] = v if @valid_config_keys.include? k.to_sym }
+  end
+
+  # Configure through yaml file
+  def self.configure_with(path_to_yaml_file)
+    config = YAML.load(IO.read(path_to_yaml_file))
+    configure(config)
+  end
+
+  def self.config
+    @config
+  end
 end
--- skellie.gemspec
+++ skellie.gemspec
@@ -26,6 +26,9 @@ Gem::Specification.new do |spec|
   spec.executables = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "activesupport"
+
   spec.add_development_dependency "standard"
+  spec.add_development_dependency "reek"
   spec.add_development_dependency "rspec"
 end
